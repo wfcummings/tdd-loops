1f2a9190e9dea2dc595d35877277c7af17fe497e
willcummings <will.cummings2@gmail.com>
2026-09-21 19:22:35 -0400

    Completed Function 3 and passes all test cases

--- index.js
+++ index.js
@@ -59,6 +59,11 @@ export function echoWithSpace(word, n) {
  */
 export function sumTo(n) {
   // TODO
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum = sum + i;
+  }
+  return sum;
 }
 
 /**
