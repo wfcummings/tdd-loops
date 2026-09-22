7a1dfe9ff63de8c52eae650f42a117fd0454a4a6
willcummings <will.cummings2@gmail.com>
2026-09-21 21:21:46 -0400

    completed function 10 and passes all test cases

--- index.js
+++ index.js
@@ -256,4 +256,15 @@ export function moveWater(colander, bucket) {
  */
 export function fizzbuzz(n) {
   // TODO
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+  }
 }
