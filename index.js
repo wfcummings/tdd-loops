260bb807acdf04a32932727a6574c56924c98669
willcummings <will.cummings2@gmail.com>
2026-09-21 20:27:15 -0400

    Completd function 6 and passes all guard cases

--- index.js
+++ index.js
@@ -128,6 +128,13 @@ export function countdown(n) {
  */
 export function sumOddsToN(n) {
   // TODO
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    if (i % 2 !== 0) {
+      sum += i;
+    }
+  }
+  return sum;
 }
 
 /**
